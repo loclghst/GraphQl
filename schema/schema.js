319c5484317f74fd3a275f168cd0f5f3b85836ec
Manish Kumar Sharma <loclghst@gmail.com>
2018-12-27 13:31:53 +0530

    create the schema file and hook it up with server.js

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,11 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLInt, GraphQLString } = graphql;
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
+
+const users = [
+  { id: "23", name: "jhon", age: 20 },
+  { id: "42", name: "Jane", age: 21 }
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -8,4 +14,21 @@ const UserType = new GraphQLObjectType({
     name: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
